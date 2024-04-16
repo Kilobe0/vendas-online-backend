eff8c8109de5532fb206d07e8b8a42219e5a3188
Kilobe0 <104333874+Kilobe0@users.noreply.github.com>
2024-04-16 12:05:20 -0300

    create service and save in memory

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService: UserService) {}
 
     @Post()
     async createUser(
         @Body() createUser: CreateUserDto
-    ) {
-        return {
-            ...createUser,
-            password: undefined,
-        };
+    ): Promise<User> {
+        return this.userService.createUser(createUser);
+    }
+
+    @Get()
+    async getAllUser(): Promise<User[]> {
+        return this.userService.getAllUsers();
     }
 }
