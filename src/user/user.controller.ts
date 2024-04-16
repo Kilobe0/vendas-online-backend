9641b6924f2edc8387cbb79a85b06e62e80571af
Kilobe0 <104333874+Kilobe0@users.noreply.github.com>
2024-04-16 14:21:26 -0300

    insert UserEntity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
 import { UserService } from './user.service';
-import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
@@ -10,12 +10,12 @@ export class UserController {
     @Post()
     async createUser(
         @Body() createUser: CreateUserDto
-    ): Promise<User> {
+    ): Promise<UserEntity> {
         return this.userService.createUser(createUser);
     }
 
     @Get()
-    async getAllUser(): Promise<User[]> {
+    async getAllUser(): Promise<UserEntity[]> {
         return this.userService.getAllUsers();
     }
 }
